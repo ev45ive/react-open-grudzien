ba2dd8f72b5e9ed014966320e8018ca617260abe
Mateusz Kulesza <mateusz@altkom>
2020-12-21 12:06:10 +0100

    Binding properities, data atributres and styles

--- src/playlists/components/PlaylistDetails.tsx
+++ src/playlists/components/PlaylistDetails.tsx
@@ -15,13 +15,18 @@ const playlist = {
 
 export const PlaylistDetails = (props: Props) => {
   return (
-    <div>
+    <div title={playlist.name} data-id={'playlist_' + playlist.id}>
       {/* dl>(dt+dd)*3 */}
       <dl>
         <dt>Name:</dt>
         <dd>{playlist.name}</dd>
         <dt>Public</dt>
-        <dd>{playlist.public ? 'Yes' : 'No'}</dd>
+        <dd style={
+          {
+            borderBottom: '1px solid currentColor',
+            color: playlist.public ? 'red' : 'blue'
+          }
+        }>{playlist.public ? 'Yes' : 'No'}</dd>
         <dt>Description:</dt>
         <dd>{playlist.description}</dd>
       </dl>
