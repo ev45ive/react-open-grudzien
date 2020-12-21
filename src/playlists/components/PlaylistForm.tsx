0eb8e447e3d06aa84f2d3f09781fb9b553941ebf
Mateusz Kulesza <mateusz@altkom>
2020-12-21 12:40:44 +0100

    Component Sate + setState + arrow function binding this

--- src/playlists/components/PlaylistForm.tsx
+++ src/playlists/components/PlaylistForm.tsx
@@ -6,33 +6,57 @@ interface Props {
 
 }
 interface State {
-
+  playlist: Playlist
 }
 
-
-const playlist: Playlist = {
-  id: 123,
-  name: 'Playlist',
-  public: true,
-  description: 'My playlist'
-}
 
 
 export default class PlaylistForm extends Component<Props, State> {
-  state = {}
+  state = {
+    playlist: {
+      id: 123,
+      name: 'Playlist',
+      public: true,
+      description: 'My playlist'
+    }
+  }
+
+  constructor(props: Props) {
+    super(props)
+    // this.handleChange = this.handleChange.bind(this)
+    // this.handleChange = (event) => this.handleChange(event)
+  }
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value)
+
+    // this.state.playlist.name = event.target.value
+    // this.setState({})
+
+    this.setState({
+      playlist: {
+        ...this.state.playlist,
+        name: event.target.value
+      }
+    })
+  }
 
   render() {
+    const { playlist } = this.state
+
     return (
       <div>
         {/* .form-group>label{Name:}+input.form-control */}
         <div className="form-group">
           <label>Name:</label>
-          <input type="text" className="form-control" defaultValue={playlist.name} />
+          <input type="text" className="form-control" defaultValue={playlist.name}
+            onChange={this.handleChange} />
+          {playlist.name.length} / 170
         </div>
 
         {/* .form-group>label>input[type=checkbox]+{ Public} */}
         <div className="form-group"><label>
-          <input type="checkbox" defaultChecked={playlist.public} /> Public</label>
+          <input type="checkbox" checked={playlist.public} /> Public</label>
         </div>
 
         {/* .form-group>label{Description:}+textarea.form-control */}
