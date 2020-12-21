a2bc1ed6fd96695187ba3abfc5bb492fc55e3299
Mateusz Kulesza <mateusz@altkom>
2020-12-21 14:16:44 +0100

    Rendering a list of components

--- src/playlists/components/PlaylistsList.tsx
+++ src/playlists/components/PlaylistsList.tsx
@@ -22,6 +22,7 @@ export default class PlaylistsList extends Component<Props, State> {
         <div className="list-group">
 
           {this.props.playlists.map((playlist, index) =>
+            // <ListItem key={playlist.id} playlist={playlist} index={index}/>
 
             <div className="list-group-item" key={playlist.id}>
               <span>{index + 1}. {playlist.name}</span>
@@ -34,3 +35,7 @@ export default class PlaylistsList extends Component<Props, State> {
     )
   }
 }
+
+// const ListItem = ({ playlist, index }: { playlist: Playlist, index: number }) => <div className="list-group-item" >
+//   <span>{index + 1}. {playlist.name}</span>
+// </div>
