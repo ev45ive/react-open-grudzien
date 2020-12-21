a1ba5463ab29f763ea7f25302facb02f7de988da
Mateusz Kulesza <mateusz@altkom>
2020-12-21 12:00:39 +0100

    Text binding + expressions

--- src/playlists/components/PlaylistDetails.tsx
+++ src/playlists/components/PlaylistDetails.tsx
@@ -3,13 +3,28 @@
 import React from 'react'
 
 interface Props {
-  
+
+}
+
+const playlist = {
+  id: 123,
+  name: 'Playlist',
+  public: true,
+  description: 'My playlist'
 }
 
 export const PlaylistDetails = (props: Props) => {
   return (
     <div>
-      details
+      {/* dl>(dt+dd)*3 */}
+      <dl>
+        <dt>Name:</dt>
+        <dd>{playlist.name}</dd>
+        <dt>Public</dt>
+        <dd>{playlist.public ? 'Yes' : 'No'}</dd>
+        <dt>Description:</dt>
+        <dd>{playlist.description}</dd>
+      </dl>
     </div>
   )
 }
